5ab4460237769b0eac3bea7d4902b2565f704957
Mike Nguyen <32407498+mikeybear@users.noreply.github.com>
2019-09-24 13:19:16 -0700

    final

--- my_viewer.h
+++ my_viewer.h
@@ -1,26 +1,225 @@
-# pragma once
 
-# include <sig/sn_poly_editor.h>
-# include <sig/sn_lines2.h>
-# include <sig/sn_primitive.h>
+# include "my_viewer.h"
 
 # include <sigogl/ui_button.h>
-# include <sigogl/ws_viewer.h>
+# include <sigogl/ui_radio_button.h>
+# include <sig/sn_primitive.h>
+# include <sig/sn_transform.h>
+# include <sig/sn_manipulator.h>
 
-// Viewer for this example:
-class MyViewer : public WsViewer
-{  protected :
-	enum MenuEv { EvNormals, EvAnimate, EvExit };
-	UiCheckButton* _nbut;
-	SnPrimitive* min;
-	bool _animating;
-   public :
-	MyViewer ( int x, int y, int w, int h, const char* l );
-	void build_ui ();
-	void add_model ( SnShape* s, GsVec p );
-	void build_scene ();
-	void show_normals ( bool view );
-	void run_animation ();
-	virtual int handle_keyboard ( const GsEvent &e ) override;
-	virtual int uievent ( int e ) override;
-};
+# include <sigogl/ws_run.h>
+
+MyViewer::MyViewer ( int x, int y, int w, int h, const char* l ) : WsViewer(x,y,w,h,l)
+{
+	_nbut=0;
+	_animating=false;
+	build_ui ();
+	build_scene ();
+}
+
+void MyViewer::build_ui ()
+{
+	UiPanel *p, *sp;
+	UiManager* uim = WsWindow::uim();
+	p = uim->add_panel ( "", UiPanel::HorizLeft );
+	p->add ( new UiButton ( "View", sp=new UiPanel() ) );
+	{	UiPanel* p=sp;
+		p->add ( _nbut=new UiCheckButton ( "Normals", EvNormals ) ); 
+	}
+	p->add ( new UiButton ( "Animate", EvAnimate ) );
+	p->add ( new UiButton ( "Exit", EvExit ) ); p->top()->separate();
+}
+
+void MyViewer::add_model ( SnShape* s, GsVec p )
+{
+	// This method demonstrates how to add some elements to our scene graph: lines,
+	// and a shape, and all in a group under a SnManipulator.
+	// Therefore we are also demonstrating the use of a manipulator to allow the user to
+	// change the position of the object with the mouse. If you do not need mouse interaction,
+	// you can just use a SnTransform to apply a transformation instead of a SnManipulator.
+	// You would then add the transform as 1st element of the group, and set g->separator(true).
+	// Your scene graph should always be carefully designed according to your application needs.
+
+	SnManipulator* manip = new SnManipulator;
+	GsMat m;
+	m.translation ( p );
+	manip->initial_mat ( m );
+
+	SnGroup* g = new SnGroup;
+	SnLines* l = new SnLines;
+	SnTransform* t = new SnTransform;
+	//m = (GsMat((float)cos(90), (float)-sin(90), 0, 0, (float)sin(90), (float)cos(90), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
+
+	l->color(GsColor::orange);
+	g->add(s);
+	g->add(l);
+	g->add(t);
+	manip->child(g);
+
+	//t->get().roty((float)0.10472);
+	t->get().translation(p);
+	manip->visible(false); // call this to turn off mouse interaction
+
+	rootg()->add(manip);
+}
+
+void MyViewer::build_scene ()
+{
+	SnPrimitive* p;
+	//SnGroup* g = new SnGroup;
+	//SnTransform* tr = (SnTransform*)((SnGroup*)((SnManipulator*)rootg()->get(0))->child())->get(1);
+
+	p = new SnPrimitive(GsPrimitive::Cylinder, 5, 5, 0.25f); // watch face
+	p->prim().material.diffuse = GsColor::darkblue;
+	p->prim().orientation = GsQuat(GsVec::i, (float)GS_PIDIV2); 
+	// rotate 90 degrees
+
+	add_model(p, GsVec(0, 0, 0));
+
+	min = new SnPrimitive(GsPrimitive::Cylinder, 0.25, 0.25, 2.5f); // hand 1
+	//g->separator(true);
+	//g->add(tr = new SnTransform);
+	//g->add(new SnModel(*p));
+	//tr->get().translation(0.5f, 0, 0);
+	//rootg()->add(g);
+	min->prim().material.diffuse = GsColor::red;
+
+	add_model(min, GsVec(0, 2.5f, 0.5f));
+
+	//p = new SnPrimitive(GsPrimitive::Cylinder, 0.5, 0, 0.5); // hand 1's arrow
+	//p->prim().material.diffuse = GsColor::red;
+
+	//add_model(p, GsVec(0, 5.5f, 0.5f));
+
+	p = new SnPrimitive(GsPrimitive::Cylinder, 0.25, 0.25, 1); // hand 2
+	p->prim().material.diffuse = GsColor::orange;
+
+	add_model(p, GsVec(0, 1, 1));
+
+	//p = new SnPrimitive(GsPrimitive::Cylinder, 0.5, 0, 0.5); // hand 2's arrow
+	//p->prim().material.diffuse = GsColor::orange;
+
+	//add_model(p, GsVec(0, 2.5f, 0.5f));
+}
+
+// Below is an example of how to control the main loop of an animation:
+void MyViewer::run_animation ()
+{
+	//if ( _animating ) return; // avoid recursive calls
+	_animating = true;
+	
+	SnManipulator* manip = rootg()->get<SnManipulator>(1); // access hand 1
+	GsMat m = manip->mat();
+	SnManipulator* manip2 = rootg()->get<SnManipulator>(3); // access hand 2
+	GsMat m2 = manip->mat();
+
+	double frdt = 1.0/60; // delta time to reach given number of frames per second
+	//double v = 1; // target velocity is 1 unit per second
+	double t=0, lt=0, t0=gs_time();
+	double increment = GS_2PI/60;
+	double second=0, minute=0, theta=0;
+
+	while (_animating == true)
+	{	while ( t-lt<frdt ) { ws_check(); t=gs_time()-t0; } // wait until it is time for next frame
+
+		second += increment;
+		if (second - GS_2PI >= 0) {
+			second = 0;
+			minute += increment;
+			if (minute - GS_2PI >= 0) {
+				minute = 0;
+			}
+		}
+		
+		GsMat transMat, transMat2, rotMat;
+		transMat.translation(GsVec(0, 0, 0.5f));
+		rotMat.rotz((float)-sin(second), (float)cos(second));
+		transMat2.translation(GsVec(0, 2.5f, 0));
+		m = transMat * rotMat * transMat2;
+		manip->initial_mat(m);
+
+		GsMat minMat, minMat2, rotMinute;
+		minMat.translation(GsVec(0, -1.5f, 0.5f));
+		rotMinute.rotz((float)-sin(minute), (float)cos(minute));
+		minMat2.translation(GsVec(0, 2.5f, 0.5f));
+		m2 = minMat * rotMinute * minMat2;
+		manip2->initial_mat(m2);
+
+		//if ( m.e24<0 ) m.e24=0; // make sure it does not go below 0
+		//manip->initial_mat ( m );
+		lt = t;
+		render(); // notify it needs redraw
+		ws_check(); // redraw now
+		
+	}
+}
+
+void MyViewer::show_normals ( bool view )
+{
+	// Note that primitives are only converted to meshes in GsModel
+	// at the first draw call.
+	GsArray<GsVec> fn;
+	SnGroup* r = (SnGroup*)root();
+	for ( int k=0; k<r->size(); k++ )
+	{	SnManipulator* manip = r->get<SnManipulator>(k);
+		SnShape* s = manip->child<SnGroup>()->get<SnShape>(0);
+		SnLines* l = manip->child<SnGroup>()->get<SnLines>(1);
+		if ( !view ) { l->visible(false); continue; }
+		l->visible ( true );
+		if ( !l->empty() ) continue; // build only once
+		l->init();
+		if ( s->instance_name()==SnPrimitive::class_name )
+		{	GsModel& m = *((SnModel*)s)->model();
+			m.get_normals_per_face ( fn );
+			const GsVec* n = fn.pt();
+			float f = 0.33f;
+			for ( int i=0; i<m.F.size(); i++ )
+			{	const GsVec& a=m.V[m.F[i].a]; l->push ( a, a+(*n++)*f );
+				const GsVec& b=m.V[m.F[i].b]; l->push ( b, b+(*n++)*f );
+				const GsVec& c=m.V[m.F[i].c]; l->push ( c, c+(*n++)*f );
+			}
+		}  
+	}
+}
+
+int MyViewer::handle_keyboard ( const GsEvent &e )
+{
+	int ret = WsViewer::handle_keyboard ( e ); // 1st let system check events
+	if ( ret ) return ret;
+
+	switch ( e.key )
+	{
+	case GsEvent::KeyEsc: {gs_exit(); return 1; }
+	case GsEvent::KeyEnter: { 
+		SnManipulator* manip = rootg()->get<SnManipulator>(1); // access hand 1
+		GsMat m = manip->mat(); 
+		GsVec p;
+		m.translation(p);
+		manip->initial_mat(m);
+		return 1; }
+		
+	case GsEvent::KeySpace: {
+				if (_animating == true) {
+					_animating = false;
+				}
+				if (_animating == false) {
+					_animating = true;
+				}
+			return 1;
+		}
+		case 'n': { bool b = !_nbut->value(); _nbut->value(b); show_normals(b); return 1; }
+		default: gsout<<"Key pressed: "<<e.key<<gsnl;
+	}
+
+	return 0;
+}
+
+int MyViewer::uievent ( int e )
+{
+	switch ( e )
+	{	case EvNormals: show_normals(_nbut->value()); return 1;
+		case EvAnimate: run_animation(); return 1;
+		case EvExit: gs_exit();
+	}
+	return WsViewer::uievent(e);
+}
